deed2b91adcbff78ee64bbc15073ccaf55e2ac1a
Mike Wendt <mike.wendt@me.com>
2020-05-12 12:52:45 -0400

    FIX Correct build tag and modify for PY versions

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -15,14 +15,15 @@ echo $DH_TOKEN | docker login --username $DH_USER --password-stdin
 
 # Get build info ready
 gpuci_logger "Preparing to build..."
-BUILD_TAG="${CUDA_VER}-${CUDA_TYPE}-${LINUX_VER}"
+BUILD_TAG="${CUDA_VER}-${IMAGE_TYPE}-${LINUX_VER}"
 BUILD_ARGS="--squash --build-arg CUDA_VER=$CUDA_VER --build-arg IMAGE_TYPE=$IMAGE_TYPE --build-arg LINUX_VER=$LINUX_VER"
 # Check if PYTHON_VER is set
 if [ -z "$PYTHON_VER" ] ; then
   gpuci_logger "PYTHON_VER is not set, skipping..."
 else
-  gpuci_logger "PYTHON_VER is set to '$PYTHON_VER', adding to build args..."
-  BUILD_ARGS="${BUILD_ARGS} --build-arg PYTHON_VER=$PYTON_VER"
+  gpuci_logger "PYTHON_VER is set to '$PYTHON_VER', adding to build args/tag..."
+  BUILD_ARGS="${BUILD_ARGS} --build-arg PYTHON_VER=${PYTHON_VER}"
+  BUILD_TAG="${BUILD_TAG}-py${PYTHON_VER}"
 fi
 gpuci_logger "Build image and tag: ${BUILD_IMAGE}:${BUILD_TAG}"
 gpuci_logger "Build args: ${BUILD_ARGS}"
