4b08f3f268e7ce07a8d3fb47b257c48e312090bd
Mike Wendt <mike.wendt@me.com>
2020-07-14 20:16:47 -0400

    ENH Add logic for PR testing
    This changes the org & repo names to include the PR ID for full chain testing

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -20,6 +20,21 @@ echo $DH_TOKEN | docker login --username $DH_USER --password-stdin &> /dev/null
 # Get build info ready
 gpuci_logger "Preparing build args and info..."
 BUILD_TAG="${CUDA_VER}-${IMAGE_TYPE}-${LINUX_VER}"
+# Check if PR build and modify BUILD_IMAGE and BUILD_TAG
+if [ ! -z "$PR_ID" ] ; then
+  echo "PR_ID is set to '$PR_ID', updating BUILD_IMAGE..."
+  BUILD_REPO=`echo $BUILD_IMAGE | tr '/' ' ' | awk '{ print $2 }'`
+  BUILD_IMAGE="gpucitesting/${BUILD_REPO}-pr${PR_ID}"
+  # Check if FROM_IMAGE to see if it is a root build
+  if [ "$FROM_IMAGE" == "gpuci/cuda" -o "$FROM_IMAGE" == "nvidia/cuda" ] ; then
+    echo ">> No need to update FROM_IMAGE, using external image..."
+  else
+    echo ">> Need to update FROM_IMAGE to use PR's version for testing..."
+    FROM_REPO=`echo $FROM_IMAGE | tr '/' ' ' | awk '{ print $2 }'`
+    FROM_IMAGE="gpucitesting/${FROM_REPO}-pr${PR_ID}"
+  fi
+fi
+# Setup initial BUILD_ARGS
 BUILD_ARGS="--squash --build-arg FROM_IMAGE=$FROM_IMAGE --build-arg CUDA_VER=$CUDA_VER --build-arg IMAGE_TYPE=$IMAGE_TYPE --build-arg LINUX_VER=$LINUX_VER"
 # Check if PYTHON_VER is set
 if [ -z "$PYTHON_VER" ] ; then
