5f44d683343afeb1e3ef64fc759a6b616307b167
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2021-02-02 23:29:12 -0500

    ENH Update gpuCI run script for PR_ID changes

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -27,7 +27,9 @@ if [ `tr -dc '.' <<<"$CUDA_VER" | awk '{ print length }'` -eq 2 ] ; then
 fi
 BUILD_TAG="${CUDA_VER}-${IMAGE_TYPE}-${LINUX_VER}"
 # Check if PR build and modify BUILD_IMAGE and BUILD_TAG
-if [ ! -z "$PR_ID" ] ; then
+if [ "$PR_ID" == "BRANCH" ] ; then
+  echo "PR_ID is set to 'BRANCH;, skipping PR updates"
+else
   echo "PR_ID is set to '$PR_ID', updating BUILD_IMAGE..."
   BUILD_REPO=`echo $BUILD_IMAGE | tr '/' ' ' | awk '{ print $2 }'`
   BUILD_IMAGE="gpucitesting/${BUILD_REPO}-pr${PR_ID}"
