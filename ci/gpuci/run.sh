b66f41d366cc1a8f03bc2502dc66286d4f49ef01
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2021-02-04 23:16:40 -0500

    ENH Switch to git install method for gpuci-tools

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -8,10 +8,10 @@ export HOME="$WORKSPACE"
 export BUILD_STACK_VER=9.3.0
 
 # Install gpuCI tools
-curl -s --retry 5 --retry-delay 5 https://raw.githubusercontent.com/rapidsai/gpuci-tools/main/install.sh -o gpuci-tools-install.sh
-bash gpuci-tools-install.sh
-export PATH="$HOME/.gpuci:$PATH"
 cd "$WORKSPACE"
+git clone https://github.com/rapidsai/gpuci-tools.git .gpuci
+chmod +x .gpuci/tools/*
+export PATH="$PWD/.gpuci/tools:$PATH"
 
 # Show env
 gpuci_logger "Exposing current environment..."
