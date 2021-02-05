ff3bc72265ccfb25350d2bfc813d0fe5b9972614
Mike Wendt <1915404+mike-wendt@users.noreply.github.com>
2021-02-04 22:21:43 -0500

    ENH Save gpuci-tools installer to a file and run
    Instead of piping the contents to bash to detect errors more easily

--- ci/gpuci/run.sh
+++ ci/gpuci/run.sh
@@ -8,7 +8,8 @@ export HOME="$WORKSPACE"
 export BUILD_STACK_VER=9.3.0
 
 # Install gpuCI tools
-curl -s --retry 5 https://raw.githubusercontent.com/rapidsai/gpuci-tools/main/install.sh | bash
+curl -s --retry 5 https://raw.githubusercontent.com/rapidsai/gpuci-tools/main/install.sh -o gpuci-tools-install.sh
+bash gpuci-tools-install.sh
 export PATH="$HOME/.gpuci:$PATH"
 cd "$WORKSPACE"
 
